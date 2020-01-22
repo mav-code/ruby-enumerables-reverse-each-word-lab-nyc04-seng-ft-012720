912a59ea68dc0a4c1e3269bb376821e122db9153
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-22 07:53:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
-def reverse_each_word(input)
-  wordarray = input.split(" ")
-  output = []
-  wordarray.each{|word| output << word.reverse}
-  output.join(" ")
-end
+# def reverse_each_word(input)
+#  wordarray = input.split(" ")
+#  output = []
+#  wordarray.each{|word| output << word.reverse}
+#  output.join(" ")
+# end
+
