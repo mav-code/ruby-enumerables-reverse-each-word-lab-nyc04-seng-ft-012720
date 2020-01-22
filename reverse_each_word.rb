ee36bd4875f736ca42536e82863ea7eb7d42c8c0
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-22 07:54:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,6 @@
 def reverse_each_word(input)
   wordarray = input.split(" ")
   output = []
-  wordarray.each{|word| output << word.reverse}
+  wordarray.collect{|word| output << word.reverse}
   output.join(" ")
 end
